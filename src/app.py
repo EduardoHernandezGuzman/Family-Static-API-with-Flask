550061031c7657be68fa2e13652d07c810fe0061
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-01-22 12:27:44 +0000

    Proyecto finalizado

--- src/app.py
+++ src/app.py
@@ -26,19 +26,36 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    return jsonify(members), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+
+@app.route('/member', methods=['POST'])
+def create_member():
+    body = request.get_json()
+    new_member = {
+        "id": body["id"],
+        "first_name": body["first_name"],
+        "age": body["age"],
+        "lucky_numbers": body["lucky_numbers"]
     }
-
-
+    jackson_family.add_member(new_member)
+    response_body = {
+        "msg": "New member successfully added",
+        "member":  new_member
+    }
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member = jackson_family.delete_member(id)
+    return jsonify({"done" : True, "deleted_member": member}), 200
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
